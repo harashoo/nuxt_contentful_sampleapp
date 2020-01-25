f96e4161711419a1b10fad0b194353963cc4251c
shogo-hr <harada.tosho.2016@gmail.com>
2020-01-25 13:19:10 +0900

    表示に必要なページを調整する

---
+++ pages/blog/_slug.vue
@@ -0,0 +1,29 @@
+<template>
+  <section class="slug">
+    <h1 class="slug_title">
+      タイトル
+    </h1>
+    <p class="slug_date">2020/1/25</p>
+    <div>
+      記事の内容
+    </div>
+  </section>
+</template>
+
+<style scoped>
+.slug {
+  max-width: 800px;
+  margin: 0 auto;
+}
+
+.slug_title {
+  font-style: 2rem;
+  font-weight: bolder;
+}
+
+.slug_date {
+  font-size: 1rem;
+  color: rgb(57, 72, 85);
+  text-align: right;
+}
+</style>
